9a12a0722db1a27dd8ebe7bda2e49806aa374af1
Austin Roberts <code@ausiv.com>
2018-07-23 21:25:34 -0500

    Add tests for web3-account

--- packages/web3-account-element/index.js
+++ packages/web3-account-element/index.js
@@ -4,15 +4,7 @@ import OrWeb3Base from '@openrelay/web3-base';
 export default class OrWeb3Account extends OrWeb3Base {
   static get is() { return "or-web3-account" };
   _render({account}) {
-    console.log("Rendering", account);
     let result = html`${account}`;
-    // <style>
-    //   .web3-account {
-    //     color: var(--web3-account-color, yellow);
-    //   }
-    // </style>
-    // <span class="web3-account">${account}</span>
-    console.log(result);
     return result;
   }
   static get properties() {
@@ -22,7 +14,6 @@ export default class OrWeb3Account extends OrWeb3Base {
   }
   ready() {
     super.ready();
-    // this.dispatchEvent(new CustomEvent('web3-child', {detail: {element: this}, bubbles: true, composed: true}));
     this.addEventListener('web3-account', e => this.setAccount(e));
   }
   setAccount(e) {
